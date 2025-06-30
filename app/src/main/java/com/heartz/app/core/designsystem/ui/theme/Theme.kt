1082a7c7b8eb1e089c717ffc03bc2bc34d2df333
sohee <144779368+sohee6989@users.noreply.github.com>
2025-06-30 09:16:32 +0900

    #4 [feat] : color & font 세팅

--- app/src/main/java/com/heartz/app/core/designsystem/ui/theme/Theme.kt
+++ app/src/main/java/com/heartz/app/core/designsystem/ui/theme/Theme.kt
@@ -1,13 +1,71 @@
 package com.heartz.app.core.designsystem.ui.theme
 
+import android.app.Activity
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.ReadOnlyComposable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.staticCompositionLocalOf
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
+
+private val LocalByeBooColors =
+    staticCompositionLocalOf<ByeBooColors> { error("No colors provided") }
+private val LocalByeBooTypography =
+    staticCompositionLocalOf<ByeBooTypography> { error("No typography provided") }
+
+object ByeBooTheme {
+    val colors: ByeBooColors
+        @Composable
+        @ReadOnlyComposable
+        get() = LocalByeBooColors.current
+
+    val typography: ByeBooTypography
+        @Composable
+        @ReadOnlyComposable
+        get() = LocalByeBooTypography.current
+}
 
 @Composable
-fun HeartzTheme(content: @Composable () -> Unit) {
-    MaterialTheme(
-        colorScheme = HeartzColorScheme,
-        typography = Typography,
+fun ProvideByeBooColorsAndTypography(
+    colors: ByeBooColors,
+    typography: ByeBooTypography,
+    content: @Composable () -> Unit
+) {
+    val provideColors = remember { colors.copy() }
+    provideColors.update(colors)
+    val provideTypography = remember { typography.copy() }
+    provideTypography.update(typography)
+
+    CompositionLocalProvider(
+        LocalByeBooColors provides provideColors,
+        LocalByeBooTypography provides provideTypography,
         content = content
     )
+}
+
+@Composable
+fun ByeBooTheme(
+    darkTheme: Boolean = false,
+    content: @Composable () -> Unit
+) {
+    val colors = ByeBooDarkColors()
+    val typography = ByeBooTypography()
+
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = true
+            WindowCompat.getInsetsController(window, view).isAppearanceLightNavigationBars = true
+        }
+    }
+
+    ProvideByeBooColorsAndTypography(colors, typography) {
+        MaterialTheme(
+            content = content
+        )
+    }
 }
