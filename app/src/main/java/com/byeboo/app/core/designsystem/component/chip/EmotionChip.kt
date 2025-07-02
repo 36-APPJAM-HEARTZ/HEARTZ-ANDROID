3d5f55ac38d9488ce0a5ba638fe066f9a65869dc
JAY <wndkdus102@naver.com>
2025-07-02 19:28:51 +0900

    [feat] EmotionChip 구현

---
+++ app/src/main/java/com/byeboo/app/core/designsystem/component/chip/EmotionChip.kt
@@ -0,0 +1,79 @@
+package com.byeboo.app.core.designsystem.component.chip
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.ColorPainter
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.byeboo.app.core.designsystem.component.tag.LargeTag
+import com.byeboo.app.core.designsystem.type.LargeTagType
+import com.byeboo.app.core.designsystem.ui.theme.ByeBooTheme
+import com.byeboo.app.core.designsystem.ui.theme.primary300
+import com.byeboo.app.core.designsystem.ui.theme.white
+import com.byeboo.app.core.designsystem.ui.theme.whiteAlpha10
+import com.byeboo.app.core.util.noRippleClickable
+
+
+@Composable
+fun EmotionChip(
+    emotionType: LargeTagType,
+    isSelected: Boolean = false,
+    onChipClick: (() -> Unit)? = null
+) {
+
+    val backgroundColor = if (isSelected) primary300 else whiteAlpha10
+
+    Column(
+        modifier = Modifier
+            .then(
+                if (onChipClick != null) {
+                    Modifier.noRippleClickable(onChipClick) // 칩 클릭 이 필 수가 아니럐ㄸ
+                } else {
+                    Modifier
+                }
+            ),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Image(
+            painter = ColorPainter(white.copy(alpha = 0.1f)),
+            contentDescription = null,
+            modifier = Modifier
+                .width(85.dp)
+                .height(84.dp)
+        )
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        LargeTag(
+            largeTagType = emotionType,
+            backgroundColor = backgroundColor
+        )
+    }
+}
+
+
+@Preview(showBackground = true, backgroundColor = 0xFF000000)
+@Composable
+private fun EmotionChipPreview() {
+    ByeBooTheme {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(20.dp),
+            modifier = Modifier
+                .fillMaxSize()
+        ) {
+            EmotionChip(emotionType = LargeTagType.EMOTION_NEUTRAL, isSelected = true) { }
+
+            EmotionChip(emotionType = LargeTagType.EMOTION_SADNESS) { }
+        }
+    }
+}
