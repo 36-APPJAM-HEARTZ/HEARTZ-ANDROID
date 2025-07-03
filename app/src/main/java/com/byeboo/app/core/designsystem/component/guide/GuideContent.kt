7c9741e97957a07cc86e134342aa2de64bc90ff7
sohee <144779368+sohee6989@users.noreply.github.com>
2025-07-03 09:48:35 +0900

    #10 [feat] : guide 컴포넌트 캐릭터 이미지 추가

--- app/src/main/java/com/byeboo/app/core/designsystem/component/guide/GuideContent.kt
+++ app/src/main/java/com/byeboo/app/core/designsystem/component/guide/GuideContent.kt
@@ -7,12 +7,12 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.style.TextAlign
@@ -54,9 +54,9 @@ fun GuideContent(
         Spacer(modifier = Modifier.height(32.dp))
 
         Image(
-            imageVector = ImageVector.vectorResource(id = R.drawable.ic_launcher_background),
+            painter = painterResource(id = R.drawable.bori_quest_start),
             contentDescription = "이미지",
-            modifier = Modifier.height(280.dp)
+            modifier = Modifier.size(width = 262.dp, height = 259.dp)
         )
 
         Spacer(modifier = Modifier.height(32.dp))
