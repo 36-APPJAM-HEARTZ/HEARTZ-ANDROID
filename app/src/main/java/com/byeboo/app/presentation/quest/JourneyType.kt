1b0f559f8b9a45472a60600bea3caeb0257090da
JAY <wndkdus102@naver.com>
2025-07-04 01:25:09 +0900

    [feat] QuestTitle 구현

---
+++ app/src/main/java/com/byeboo/app/presentation/quest/JourneyType.kt
@@ -0,0 +1,13 @@
+package com.byeboo.app.presentation.quest
+
+enum class JourneyType(
+    val title: String
+) {
+    EMOTION_FACING(
+        title = "감정 직면 여정"
+    ),
+    EMOTION_PROCESSING(
+        title = "감정 정리 여정"
+    )
+
+}
