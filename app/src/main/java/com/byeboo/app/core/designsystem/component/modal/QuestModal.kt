aa9e62d996450a2abbe35ca2bba2de3a66d9d895
sohee <144779368+sohee6989@users.noreply.github.com>
2025-07-04 10:01:58 +0900

    #10 [feat] : quest modal 컴포넌트 캐릭터 추가

--- app/src/main/java/com/byeboo/app/core/designsystem/component/modal/QuestModal.kt
+++ app/src/main/java/com/byeboo/app/core/designsystem/component/modal/QuestModal.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
@@ -15,8 +16,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.style.TextAlign
@@ -48,9 +48,11 @@ fun QuestModal(
             verticalArrangement = Arrangement.Center
         ) {
             Image(
-                imageVector = ImageVector.vectorResource(id = R.drawable.ic_launcher_background),
+                painter = painterResource(id = R.drawable.bori_quest_banner),
                 contentDescription = "이미지",
-                Modifier.height(61.dp)
+                Modifier
+                    .fillMaxWidth()
+                    .height(58.dp)
             )
 
             Spacer(modifier = Modifier.height((17.5).dp))
