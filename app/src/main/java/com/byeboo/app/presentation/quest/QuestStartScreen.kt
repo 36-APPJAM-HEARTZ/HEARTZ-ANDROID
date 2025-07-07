c0f32b2d06ea0b99b6a9ebc57bfe5010c7b542e5
sohee <144779368+sohee6989@users.noreply.github.com>
2025-07-07 23:58:32 +0900

    #21 [mod] : QuestStartScreen의 Box 제거

--- app/src/main/java/com/byeboo/app/presentation/quest/QuestStartScreen.kt
+++ app/src/main/java/com/byeboo/app/presentation/quest/QuestStartScreen.kt
@@ -1,7 +1,6 @@
 package com.byeboo.app.presentation.quest
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -9,7 +8,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -27,26 +25,24 @@ fun QuestStartScreen(
     modifier: Modifier = Modifier,
     viewModel: QuestStartViewModel = hiltViewModel()
 ) {
-    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+    val uiState by viewModel.state.collectAsStateWithLifecycle()
 
-    Box(
+    Column(
         modifier = modifier
             .fillMaxSize()
             .background(color = ByeBooTheme.colors.black)
-            .padding(bottom = 8.dp)
+            .padding(horizontal = 24.dp)
     ) {
-        Column(
-            modifier = Modifier
-        ) {
-            ByeBooTopBar(onNavigateBack = onNavigateBack)
+        ByeBooTopBar(onNavigateBack = onNavigateBack)
 
-            Spacer(modifier = Modifier.height(11.dp))
+        Spacer(modifier = Modifier.height(11.dp))
 
-            GuideContent(
-                nickname = uiState.nickname,
-                guideText = "님의 상황에 꼭 맞춘\n${uiState.journeyName} 여정의 퀘스트 30개를 드릴게요.\n\n제가 드리는 퀘스트와 함꼐\n이별을 극복해나가요!"
-            )
-        }
+        GuideContent(
+            nickname = uiState.nickname,
+            guideText = "님의 상황에 꼭 맞춘\n${uiState.journeyName} 여정의 퀘스트 30개를 드릴게요.\n\n제가 드리는 퀘스트와 함꼐\n이별을 극복해나가요!"
+        )
+
+        Spacer(modifier = Modifier.weight(1f))
 
         ByeBooButton(
             onClick = onStartButtonClick,
@@ -54,9 +50,9 @@ fun QuestStartScreen(
             buttonTextColor = ByeBooTheme.colors.white,
             buttonBackgroundColor = ByeBooTheme.colors.primary300,
             modifier = Modifier
-                .padding(horizontal = 24.dp)
-                .align(Alignment.BottomCenter)
         )
+
+        Spacer(modifier = Modifier.padding(bottom = 56.dp))
     }
 }
 
