9b5c08ee4d1008386aed3ea2392bfa7946235b2e
JAY <wndkdus102@naver.com>
2025-07-07 12:00:02 +0900

    [chore] 변수명 변경

--- app/src/main/java/com/byeboo/app/presentation/quest/component/type/JourneyType.kt
+++ app/src/main/java/com/byeboo/app/presentation/quest/component/type/JourneyType.kt
@@ -3,11 +3,10 @@ package com.byeboo.app.presentation.quest.component.type
 enum class JourneyType(
     val title: String
 ) {
-    EMOTION_FACING(
+    FACE_EMOTION(
         title = "감정 직면 여정"
     ),
-    EMOTION_PROCESSING(
+    PROCESS_EMOTION(
         title = "감정 정리 여정"
     )
-
 }
