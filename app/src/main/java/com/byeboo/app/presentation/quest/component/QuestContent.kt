50808452b749f83f955b697ded1ac42e6c32d66e
sohee <144779368+sohee6989@users.noreply.github.com>
2025-07-09 02:47:21 +0900

    #27 [mod] : QuestContent 아이콘 패딩 삭제

--- app/src/main/java/com/byeboo/app/presentation/quest/component/QuestContent.kt
+++ app/src/main/java/com/byeboo/app/presentation/quest/component/QuestContent.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -38,9 +39,10 @@ fun QuestContent(
             Icon(
                 imageVector = icon,
                 contentDescription = "title icon",
-                modifier = Modifier.padding(end = 8.dp),
                 tint = Color.Unspecified
             )
+
+            Spacer(modifier = Modifier.width(8.dp))
 
             Text(
                 text = titleText,
