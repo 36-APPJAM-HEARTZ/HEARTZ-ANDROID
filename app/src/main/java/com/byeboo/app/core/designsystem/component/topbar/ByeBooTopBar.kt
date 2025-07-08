96f9f5e3ef0d11729dbe6478b52a6bb6c643f3e7
sohee <144779368+sohee6989@users.noreply.github.com>
2025-07-08 23:20:44 +0900

    #27 [mod] : topbar 배경색 수정

--- app/src/main/java/com/byeboo/app/core/designsystem/component/topbar/ByeBooTopBar.kt
+++ app/src/main/java/com/byeboo/app/core/designsystem/component/topbar/ByeBooTopBar.kt
@@ -14,6 +14,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.style.TextAlign
@@ -34,7 +35,7 @@ fun ByeBooTopBar(
         modifier = modifier
             .fillMaxWidth()
             .height(56.dp)
-            .background(ByeBooTheme.colors.black)
+            .background(Color.Transparent)
     ) {
         Row(
             modifier = Modifier
