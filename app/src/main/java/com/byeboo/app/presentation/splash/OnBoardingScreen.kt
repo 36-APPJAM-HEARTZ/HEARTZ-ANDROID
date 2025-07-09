451429ffe11d52f4caa85935bc18c1ecdc517d7c
JAY <wndkdus102@naver.com>
2025-07-09 16:22:41 +0900

    #29 [feat] : OnBoarding UI 구현

---
+++ app/src/main/java/com/byeboo/app/presentation/splash/OnBoardingScreen.kt
@@ -0,0 +1,166 @@
+package com.byeboo.app.presentation.splash
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.byeboo.app.R
+import com.byeboo.app.core.designsystem.component.button.ByeBooButton
+import com.byeboo.app.core.designsystem.ui.theme.ByeBooTheme
+
+@Composable
+fun OnBoardingScreen(
+    modifier: Modifier = Modifier,
+    viewModel: OnBoardingViewModel = hiltViewModel()
+) {
+
+    val pageIndex by viewModel.pageIndex
+    val contents = viewModel.currentContents()
+
+
+    val buttonText = if (pageIndex == 2) "시작하기" else "다음으로"
+
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(ByeBooTheme.colors.primary900),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Row(
+            modifier = Modifier
+                .padding(start = 24.dp, end = 24.dp, top = 67.dp, bottom = 16.dp),
+            verticalAlignment = Alignment.CenterVertically
+
+        ) {
+
+            Text(
+                text = viewModel.showPageNumber(),
+                color = ByeBooTheme.colors.primary200,
+                style = ByeBooTheme.typography.body5
+            )
+
+            Spacer(modifier = Modifier.weight(1f))
+
+            Row(
+                modifier = Modifier.clickable { viewModel.skipPage() },
+                verticalAlignment = Alignment.CenterVertically
+
+            ) {
+                Text(
+                    text = "Skip",
+                    color = ByeBooTheme.colors.primary200,
+                    style = ByeBooTheme.typography.body5
+                )
+
+
+                Icon(
+                    imageVector = ImageVector.vectorResource(id = R.drawable.ic_right),
+                    contentDescription = "next",
+                    tint = ByeBooTheme.colors.primary200,
+                    modifier = Modifier.size(12.dp)
+                )
+            }
+        }
+
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 45.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+
+
+            contents.forEach { content ->
+
+                Spacer(modifier = Modifier.height(12.dp))
+
+                Image(
+                    painter = painterResource(id = content.imageRes),
+                    contentDescription = "OnBoarding image",
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    contentScale = ContentScale.FillWidth,
+                )
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+
+                Text(
+                    text = content.title,
+                    color = ByeBooTheme.colors.primary100,
+                    style = ByeBooTheme.typography.body3,
+                    textAlign = TextAlign.Center
+                )
+
+                Spacer(modifier = Modifier.height(12.dp))
+
+
+
+            }
+        }
+
+        Spacer(modifier = Modifier.weight(1f))
+
+
+        ByeBooButton(
+            modifier = Modifier
+                    .padding(bottom = 56.dp)
+                    .padding(horizontal = 24.dp),
+                buttonText = buttonText,
+                onClick = {viewModel.nextPage()},
+                buttonTextColor = ByeBooTheme.colors.white,
+                buttonBackgroundColor = ByeBooTheme.colors.primary300
+
+        )
+
+
+
+    }
+
+
+
+
+}
+
+
+
+
+
+
+
+
+@Composable
+fun ImageContent() {
+
+}
+
+@Preview
+@Composable
+private fun OnBoardingScreenPreview() {
+
+    OnBoardingScreen()
+
+}
---
+++ app/src/main/java/com/byeboo/app/presentation/splash/OnBoardingState.kt
@@ -0,0 +1,6 @@
+package com.byeboo.app.presentation.splash
+
+data class OnBoardingState(
+    val title: String,
+    val imageRes: Int
+)
