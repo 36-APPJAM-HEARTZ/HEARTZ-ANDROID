cf96c76192d8c58eb42887b6c00f03e369266a9f
JAY <wndkdus102@naver.com>
2025-07-10 16:37:49 +0900

    #24 [chore] : import 제거

--- app/src/main/java/com/byeboo/app/presentation/quest/component/QuestTitle.kt
+++ app/src/main/java/com/byeboo/app/presentation/quest/component/QuestTitle.kt
@@ -11,7 +11,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.byeboo.app.core.designsystem.component.tag.SmallTag
 import com.byeboo.app.core.designsystem.ui.theme.ByeBooTheme
@@ -60,5 +59,4 @@ fun  QuestTitle(
         )
     }
 }
-
 
