8905be2c6f3a9ecbfecd41b9080393580d33ea1a
JAY <wndkdus102@naver.com>
2025-07-10 23:56:39 +0900

    #37 [chore] : image Box 크기 수정

--- app/src/main/java/com/byeboo/app/presentation/quest/behavior/QuestBehaviorCompleteScreen.kt
+++ app/src/main/java/com/byeboo/app/presentation/quest/behavior/QuestBehaviorCompleteScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -14,6 +15,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -56,7 +58,7 @@ fun QuestBehaviorCompleteScreen(
             Row(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(top = 67.dp), // optional
+                    .padding(top = 67.dp),
                 horizontalArrangement = Arrangement.End
             ) {
                 Icon(
@@ -68,6 +70,8 @@ fun QuestBehaviorCompleteScreen(
                     }
                 )
             }
+
+            Spacer(modifier = Modifier.height(24.dp))
         }
 
         item {
@@ -92,8 +96,8 @@ fun QuestBehaviorCompleteScreen(
             ) {
                 Box(
                     modifier = Modifier
-                        .width(312.dp)
-                        .height(312.dp)
+                        .fillMaxWidth()
+                        .aspectRatio(1f)
                         .clip(RoundedCornerShape(12.dp))
                 ) {
                     selectedImageUri?.let { uri ->
