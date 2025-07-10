6ba40f251447b283957666e11d7e35dc056b4ef8
fredLeeJH <fred54755475@gmail.com>
2025-07-10 17:00:44 +0900

    #32 [feat] : auth nav setting

--- app/src/main/java/com/byeboo/app/presentation/auth/userinfo/navigation/AuthNavigation.kt
+++ app/src/main/java/com/byeboo/app/presentation/auth/userinfo/navigation/AuthNavigation.kt
@@ -5,6 +5,8 @@ import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavOptions
 import androidx.navigation.compose.composable
 import com.byeboo.app.core.navigation.Route
+import com.byeboo.app.presentation.auth.loading.LoadingScreen
+import com.byeboo.app.presentation.auth.onboarding.OnboardingScreen
 import com.byeboo.app.presentation.auth.userinfo.UserInfoScreen
 import kotlinx.serialization.Serializable
 
@@ -12,17 +14,39 @@ fun NavController.navigateToUserInfo(navOptions: NavOptions? = null) {
     navigate(UserInfo, navOptions)
 }
 
-fun NavGraphBuilder.userInfoGraph(
+fun NavController.navigateToLoading(navOptions: NavOptions? = null) {
+    navigate(Loading, navOptions)
+}
+
+fun NavGraphBuilder.authGraph(
+    navigateToUserInfo: () -> Unit,
     navigateToOnboarding: () -> Unit,
-    navigateToLoading: () -> Unit
+    navigateToLoading: () -> Unit,
+    navigateToHomeOnboarding: () -> Unit,
 ) {
+    composable<Onboarding> {
+        OnboardingScreen(
+            navigateToUserInfo = navigateToUserInfo
+        )
+    }
     composable<UserInfo> {
         UserInfoScreen(
             navigateToOnboarding = navigateToOnboarding,
             navigateToLoading = navigateToLoading
         )
     }
+    composable<Loading> {
+        LoadingScreen(
+            navigateToHomeOnboarding = navigateToHomeOnboarding
+        )
+    }
 }
 
 @Serializable
+data object Onboarding : Route
+
+@Serializable
 data object UserInfo : Route
+
+@Serializable
+data object Loading : Route
