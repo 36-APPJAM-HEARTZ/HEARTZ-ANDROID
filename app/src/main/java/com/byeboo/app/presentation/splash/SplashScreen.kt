f2f47eeb39c0c4633d4e03db0c36bfd91db05e1e
JAY <wndkdus102@naver.com>
2025-07-12 00:44:28 +0900

    #42 [feat] : background img 수정

--- app/src/main/java/com/byeboo/app/presentation/splash/SplashScreen.kt
+++ app/src/main/java/com/byeboo/app/presentation/splash/SplashScreen.kt
@@ -1,18 +1,10 @@
 package com.byeboo.app.presentation.splash
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -36,7 +28,7 @@ fun SplashScreen(
 
 
         Image(
-            painter = painterResource(R.drawable.bg_userinfo),
+            painter = painterResource(R.drawable.img_splash_background),
             contentDescription = null,
             modifier = modifier.fillMaxSize(),
             contentScale = ContentScale.Crop
