3f0655d45f9ee7820bd838b09c0982b14d684ca7
fredLeeJH <fred54755475@gmail.com>
2025-07-13 20:15:35 +0900

    #50 [chore] : change tip screen

--- app/src/main/java/com/byeboo/app/presentation/quest/QuestTipScreen.kt
+++ app/src/main/java/com/byeboo/app/presentation/quest/QuestTipScreen.kt
@@ -1,6 +1,5 @@
 package com.byeboo.app.presentation.quest
 
-import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
