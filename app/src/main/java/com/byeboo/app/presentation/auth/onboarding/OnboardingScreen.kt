ff2594acd83dff6663921468dd38fc4a5c5da4a7
JAY <wndkdus102@naver.com>
2025-07-13 14:03:15 +0900

    #51 [fix] : UI 수정

--- app/src/main/java/com/byeboo/app/presentation/auth/onboarding/OnboardingScreen.kt
+++ app/src/main/java/com/byeboo/app/presentation/auth/onboarding/OnboardingScreen.kt
@@ -1,8 +1,8 @@
 package com.byeboo.app.presentation.auth.onboarding
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -23,6 +23,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -44,98 +45,105 @@ fun OnboardingScreen(
 
     val buttonText = if (pageIndex == 2) "시작하기" else "다음으로"
 
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(ByeBooTheme.colors.primary900),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Row(
-            modifier = Modifier
-                .padding(start = 24.dp, end = 24.dp, top = padding + 27.dp, bottom = 16.dp),
-            verticalAlignment = Alignment.CenterVertically
-
+    Box(modifier = Modifier.fillMaxSize()) {
+        Image(
+            painter = painterResource(R.drawable.img_onboarding_background),
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.fillMaxSize()
+        )
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text(
-                text = viewModel.showPageNumber(),
-                color = ByeBooTheme.colors.primary200,
-                style = ByeBooTheme.typography.body5
-            )
-
-            Spacer(modifier = Modifier.weight(1f))
-
             Row(
-                modifier = Modifier.clickable { viewModel.skipPage(navigateToUserInfo) },
-                verticalAlignment = Alignment.CenterVertically
+                modifier = Modifier.padding(
+                    start = 24.dp,
+                    end = 24.dp,
+                    top = padding,
+                    bottom = 16.dp
+                ), verticalAlignment = Alignment.CenterVertically
 
             ) {
                 Text(
-                    text = "Skip",
-                    color = ByeBooTheme.colors.primary200,
-                    style = ByeBooTheme.typography.body5.copy(
-                        textDecoration = TextDecoration.Underline
+                    text = viewModel.showPageNumber(),
+                    color = ByeBooTheme.colors.primary300,
+                    style = ByeBooTheme.typography.body5
+                )
+
+                Spacer(modifier = Modifier.weight(1f))
+
+                Row(
+                    modifier = Modifier.clickable { viewModel.skipPage(navigateToUserInfo) },
+                    verticalAlignment = Alignment.CenterVertically
+
+                ) {
+                    Text(
+                        text = "Skip",
+                        color = ByeBooTheme.colors.primary300,
+                        style = ByeBooTheme.typography.body5.copy(
+                            textDecoration = TextDecoration.Underline
+                        )
                     )
-                )
 
-                Icon(
-                    imageVector = ImageVector.vectorResource(id = R.drawable.ic_right),
-                    contentDescription = "next",
-                    tint = ByeBooTheme.colors.primary200,
-                    modifier = Modifier.size(12.dp)
-                )
-            }
-        }
-
-        Column(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(horizontal = 45.dp),
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            contents.forEach { content ->
-
-                Spacer(modifier = Modifier.height(12.dp))
-
-                Image(
-                    painter = painterResource(id = content.imageRes),
-                    contentDescription = "OnBoarding image",
-                    modifier = Modifier
-                        .fillMaxWidth(),
-                    contentScale = ContentScale.FillWidth
-                )
-
-                Spacer(modifier = Modifier.height(pageSpace))
-
-                Text(
-                    text = content.title,
-                    color = ByeBooTheme.colors.primary100,
-                    style = ByeBooTheme.typography.body3,
-                    textAlign = TextAlign.Center
-                )
-
-                Spacer(modifier = Modifier.height(12.dp))
-            }
-        }
-
-        Spacer(modifier = Modifier.weight(1f))
-
-        ByeBooButton(
-            modifier = Modifier.padding(
-                start = 24.dp,
-                end = 24.dp,
-                bottom = padding
-            ),
-            buttonText = buttonText,
-            onClick = {
-                if (pageIndex == 2) {
-                    navigateToUserInfo()
-                } else {
-                    viewModel.nextPage()
+                    Icon(
+                        imageVector = ImageVector.vectorResource(id = R.drawable.ic_right),
+                        contentDescription = "next",
+                        tint = ByeBooTheme.colors.primary200,
+                        modifier = Modifier.size(12.dp)
+                    )
                 }
-            },
-            buttonTextColor = ByeBooTheme.colors.white,
-            buttonBackgroundColor = ByeBooTheme.colors.primary300
+            }
 
-        )
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 45.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                contents.forEach { content ->
+
+                    Spacer(modifier = Modifier.height(12.dp))
+
+                    Image(
+                        painter = painterResource(id = content.imageRes),
+                        contentDescription = "OnBoarding image",
+                        modifier = Modifier.fillMaxWidth(),
+                        contentScale = ContentScale.FillWidth
+                    )
+
+                    Spacer(modifier = Modifier.height(pageSpace))
+
+                    Text(
+                        text = content.title,
+                        color = ByeBooTheme.colors.primary900,
+                        style = ByeBooTheme.typography.body3,
+                        textAlign = TextAlign.Center
+                    )
+
+                    Spacer(modifier = Modifier.height(12.dp))
+                }
+            }
+
+            Spacer(modifier = Modifier.weight(1f))
+
+            ByeBooButton(
+                modifier = Modifier.padding(
+                    start = 24.dp,
+                    end = 24.dp,
+                    bottom = padding
+                ),
+                buttonText = buttonText,
+                onClick = {
+                    if (pageIndex == 2) {
+                        navigateToUserInfo()
+                    } else {
+                        viewModel.nextPage()
+                    }
+                },
+                buttonTextColor = ByeBooTheme.colors.white,
+                buttonBackgroundColor = ByeBooTheme.colors.primary300
+
+            )
+        }
     }
 }
