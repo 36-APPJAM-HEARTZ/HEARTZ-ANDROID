b86b80ecde8a8055cff7d3204405ad461825c95b
fredLeeJH <fred54755475@gmail.com>
2025-07-13 20:06:16 +0900

    #50 [feat] : add userId to Header

---
+++ app/src/main/java/com/byeboo/app/core/network/AuthInterceptor.kt
@@ -0,0 +1,27 @@
+package com.byeboo.app.core.network
+
+import com.byeboo.app.domain.repository.TokenRepository
+import kotlinx.coroutines.runBlocking
+import javax.inject.Inject
+import okhttp3.Interceptor
+import okhttp3.Response
+
+class AuthInterceptor @Inject constructor(
+    private val tokenRepository: TokenRepository
+) : Interceptor {
+
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val request = chain.request()
+        val userId = runBlocking { tokenRepository.getUserId() }
+
+        val newRequest = request.newBuilder()
+            .addHeader(USER_ID_HEADER_KEY, userId?.toString() ?: "")
+            .build()
+        return chain.proceed(newRequest)
+    }
+
+    companion object {
+        const val USER_ID_HEADER_KEY = "userId"
+    }
+}
+
