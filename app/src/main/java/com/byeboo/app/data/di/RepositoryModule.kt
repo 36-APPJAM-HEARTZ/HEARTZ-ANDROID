69bd580f3d9a12cdb1e6125c47708751ed7a009a
JAY <wndkdus102@naver.com>
2025-07-14 00:03:46 +0900

    #53 [feat] : module 생성

--- app/src/main/java/com/byeboo/app/data/di/RepositoryModule.kt
+++ app/src/main/java/com/byeboo/app/data/di/RepositoryModule.kt
@@ -2,8 +2,12 @@ package com.byeboo.app.data.di
 
 import com.byeboo.app.data.repositoryimpl.DummyRepositoryImpl
 import com.byeboo.app.data.repositoryimpl.auth.UserRepositoryImpl
+import com.byeboo.app.data.repositoryimpl.quest.behavior.QuestDetailBehaviorRepositoryImpl
+import com.byeboo.app.data.repositoryimpl.quest.recording.QuestDetailRecordingRepositoryImpl
 import com.byeboo.app.domain.repository.DummyRepository
 import com.byeboo.app.domain.repository.UserRepository
+import com.byeboo.app.domain.repository.quest.QuestDetailBehaviorRepository
+import com.byeboo.app.domain.repository.quest.QuestDetailRecordingRepository
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -20,4 +24,12 @@ abstract class RepositoryModule {
     @Binds
     @Singleton
     abstract fun bindsUserRepository(userRepositoryImpl: UserRepositoryImpl): UserRepository
+
+    @Binds
+    @Singleton
+    abstract fun bindQuestDetailBehaviorRepository(questDetailBehaviorRepositoryImpl: QuestDetailBehaviorRepositoryImpl) : QuestDetailBehaviorRepository
+
+    @Binds
+    @Singleton
+    abstract fun bindQuestDetailRecordingRepository(questDetailRecordingRepositoryImpl: QuestDetailRecordingRepositoryImpl): QuestDetailRecordingRepository
 }
--- app/src/main/java/com/byeboo/app/data/di/ServiceModule.kt
+++ app/src/main/java/com/byeboo/app/data/di/ServiceModule.kt
@@ -2,6 +2,7 @@ package com.byeboo.app.data.di
 
 import com.byeboo.app.data.service.DummyService
 import com.byeboo.app.data.service.auth.UserService
+import com.byeboo.app.data.service.quest.QuestDetailService
 import com.kakao.sdk.user.model.User
 import dagger.Module
 import dagger.Provides
@@ -23,6 +24,12 @@ object ServiceModule {
     @Singleton
     fun providesUserService(retrofit: Retrofit): UserService = retrofit.create(
         UserService::class.java
+    )
+
+    @Provides
+    @Singleton
+    fun providesQuestDetailService(retrofit: Retrofit): QuestDetailService = retrofit.create(
+        QuestDetailService::class.java
     )
 
 }
