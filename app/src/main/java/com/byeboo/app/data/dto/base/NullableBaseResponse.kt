d89ec044fda737eb2136a12d687550d941e62946
fredLeeJH <fred54755475@gmail.com>
2025-07-13 20:13:01 +0900

    #50 [feat] : add dto

---
+++ app/src/main/java/com/byeboo/app/data/dto/base/NullableBaseResponse.kt
@@ -0,0 +1,14 @@
+package com.byeboo.app.data.dto.base
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class NullableBaseResponse<T>(
+    @SerialName("success")
+    val success: Boolean,
+    @SerialName("message")
+    val message: String,
+    @SerialName("data")
+    val data: T? = null
+)
