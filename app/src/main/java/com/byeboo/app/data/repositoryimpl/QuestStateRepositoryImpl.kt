78c0fea0d9cebfaf27acfce7304840fbd79503a7
fredLeeJH <fred54755475@gmail.com>
2025-07-13 20:13:34 +0900

    #50 [feat] : add quest di

---
+++ app/src/main/java/com/byeboo/app/data/repositoryimpl/QuestStateRepositoryImpl.kt
@@ -0,0 +1,61 @@
+package com.byeboo.app.data.repositoryimpl
+
+import com.byeboo.app.data.datasource.local.UserLocalDataSource
+import com.byeboo.app.data.datasource.remote.QuestStateDataSource
+import com.byeboo.app.data.mapper.toDomain
+import com.byeboo.app.domain.model.quest.QuestDialogue
+import com.byeboo.app.domain.model.quest.QuestStateModel
+import com.byeboo.app.domain.repository.QuestStateRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import javax.inject.Inject
+
+class QuestStateRepositoryImpl @Inject constructor(
+    private val questStateDataSource: QuestStateDataSource,
+    private val userLocalDataSource: UserLocalDataSource
+) : QuestStateRepository {
+
+
+    override suspend fun updateQuestState() {
+        runCatching {
+            questStateDataSource.updateQuestState()
+        }
+    }
+
+    override suspend fun updateUserJourney(journey: String) {
+        userLocalDataSource.saveJourney(journey)
+    }
+
+    override suspend fun getUserJourney(): String? {
+        return userLocalDataSource.getJourney()
+    }
+
+    override suspend fun getQuestDialogue(): Result<QuestDialogue> {
+        return runCatching {
+            val response = questStateDataSource.getQuestDialogue()
+            response.data.toDomain()
+        }
+    }
+
+
+    override suspend fun setQuestStarted(started: Boolean) {
+        userLocalDataSource.setQuestStarted(started)
+    }
+
+    override suspend fun isQuestStarted(): Boolean {
+        return userLocalDataSource.isQuestStarted()
+    }
+
+
+    override fun getQuestCount(): Flow<QuestStateModel> = flow {
+        while (true) {
+            runCatching {
+                questStateDataSource.getQuestCount()
+            }.onSuccess { dto ->
+                emit(dto.data.toDomain())
+            }
+            //추후 바텀시트 버튼 완료시 프로그래스 상태를 업데이트하는 로직으로 변경 예정
+            kotlinx.coroutines.delay(30000L)
+        }
+    }
+}
--- app/src/main/java/com/byeboo/app/presentation/quest/QuestStartViewModel.kt
+++ app/src/main/java/com/byeboo/app/presentation/quest/QuestStartViewModel.kt
@@ -2,6 +2,8 @@ package com.byeboo.app.presentation.quest
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.byeboo.app.domain.repository.QuestStateRepository
+import com.byeboo.app.domain.repository.UserRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -9,18 +11,24 @@ import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
+import timber.log.Timber
 import javax.inject.Inject
 
 @HiltViewModel
-class QuestStartViewModel @Inject constructor() : ViewModel() {
+class QuestStartViewModel @Inject constructor(
+    private val questStateRepository: QuestStateRepository
+
+) : ViewModel() {
     private val _state = MutableStateFlow(QuestStartState())
     val state: StateFlow<QuestStartState> = _state.asStateFlow()
 
     private val _sideEffect = MutableSharedFlow<QuestStartSideEffect>()
     val sideEffect: SharedFlow<QuestStartSideEffect> = _sideEffect
 
-    fun onStartClick(){
+    fun onStartClick() {
         viewModelScope.launch {
+            questStateRepository.updateQuestState()
+            questStateRepository.setQuestStarted(true)
             _sideEffect.emit(QuestStartSideEffect.NavigateToQuest)
         }
     }
