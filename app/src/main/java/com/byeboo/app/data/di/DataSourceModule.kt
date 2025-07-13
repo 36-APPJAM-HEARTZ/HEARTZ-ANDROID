7eb722ca2ea67ad41718e25bdbee2010b297d49b
fredLeeJH <fred54755475@gmail.com>
2025-07-13 20:07:05 +0900

    #50 [feat] : add questStateDataSource

--- app/src/main/java/com/byeboo/app/data/di/DataSourceModule.kt
+++ app/src/main/java/com/byeboo/app/data/di/DataSourceModule.kt
@@ -2,9 +2,11 @@ package com.byeboo.app.data.di
 
 import com.byeboo.app.data.datasource.local.DummyLocalDataSource
 import com.byeboo.app.data.datasource.remote.DummyRemoteDataSource
+import com.byeboo.app.data.datasource.remote.QuestStateDataSource
 import com.byeboo.app.data.datasource.remote.UserRemoteDataSource
 import com.byeboo.app.data.datasourceimpl.local.DummyLocalDataSourceImpl
 import com.byeboo.app.data.datasourceimpl.remote.DummyRemoteDataSourceImpl
+import com.byeboo.app.data.datasourceimpl.remote.QuestStateDataSourceImpl
 import com.byeboo.app.data.datasourceimpl.remote.UserRemoteDataSourceImpl
 import dagger.Binds
 import dagger.Module
@@ -25,5 +27,9 @@ abstract class DataSourceModule {
 
     @Binds
     @Singleton
-    abstract fun bindUserRemoteDataSource(impl: UserRemoteDataSourceImpl) : UserRemoteDataSource
+    abstract fun bindUserRemoteDataSource(impl: UserRemoteDataSourceImpl): UserRemoteDataSource
+
+    @Binds
+    @Singleton
+    abstract fun bindsQuestStateDataSource(impl: QuestStateDataSourceImpl): QuestStateDataSource
 }
