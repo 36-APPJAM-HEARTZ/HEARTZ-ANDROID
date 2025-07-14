d5b9b3a1ad80803d00e9c13e2ddb937a600095a5
JAY <wndkdus102@naver.com>
2025-07-14 22:37:55 +0900

    #60 [feat] : image url repository 생성

--- app/src/main/java/com/byeboo/app/data/repositoryimpl/QuestBehaviorAnswerRepositoryImpl.kt
+++ app/src/main/java/com/byeboo/app/data/repositoryimpl/QuestBehaviorAnswerRepositoryImpl.kt
@@ -1,9 +1,13 @@
 package com.byeboo.app.data.repositoryimpl
 
+import androidx.compose.ui.autofill.ContentType
 import com.byeboo.app.data.datasource.remote.QuestBehaviorAnswerDataSource
 import com.byeboo.app.data.dto.request.QuestBehaviorAnswerRequestDto
 import com.byeboo.app.data.dto.request.QuestSignedUrlRequestDto
 import com.byeboo.app.domain.repository.QuestBehaviorAnswerRepository
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.RequestBody.Companion.toRequestBody
+import java.io.IOException
 import javax.inject.Inject
 
 class QuestBehaviorAnswerRepositoryImpl @Inject constructor(
@@ -15,6 +19,18 @@ class QuestBehaviorAnswerRepositoryImpl @Inject constructor(
             val response = questBehaviorAnswerDataSource.postQuestSignedUrl(request)
             response.data.signedUrl
         }.getOrThrow()
+    }
+
+    override suspend fun putImageToSignedUrl(
+        signUrl: String,
+        imageBytes: ByteArray,
+        contentType: String
+    ) {
+        val requestBody = imageBytes.toRequestBody(contentType.toMediaTypeOrNull())
+        val response = questBehaviorAnswerDataSource.putImageToSignedUrl(signUrl, requestBody)
+        if (!response.isSuccessful) {
+            throw IOException("Image upload failed with code ${response.code()}")
+        }
     }
 
     override suspend fun postQuestBehaviorAnswer(
--- app/src/main/java/com/byeboo/app/domain/repository/QuestBehaviorAnswerRepository.kt
+++ app/src/main/java/com/byeboo/app/domain/repository/QuestBehaviorAnswerRepository.kt
@@ -6,6 +6,7 @@ import com.byeboo.app.data.dto.request.QuestSignedUrlRequestDto
 interface QuestBehaviorAnswerRepository {
     suspend fun postQuestSignedUrl(request: QuestSignedUrlRequestDto): String
 
+    suspend fun putImageToSignedUrl(signUrl: String, imageBytes: ByteArray, contentType: String)
     suspend fun postQuestBehaviorAnswer(questId: Long, request: QuestBehaviorAnswerRequestDto)
 }
 
