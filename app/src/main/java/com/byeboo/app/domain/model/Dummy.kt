ba609b7ba09297b34190d168fb150adab6ce41af
fredLeeJH <fred54755475@gmail.com>
2025-07-14 20:03:45 +0900

    #59 [delete] : delete dummy

--- app/src/main/java/com/byeboo/app/domain/model/Dummy.kt
+++
@@ -1,6 +0,0 @@
-package com.byeboo.app.domain.model
-
-data class Dummy(
-    val id: Int,
-    val email: String
-)
--- app/src/main/java/com/byeboo/app/domain/repository/DummyRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.byeboo.app.domain.repository
-
-import com.byeboo.app.domain.model.Dummy
-import com.byeboo.app.domain.model.DummyResultModel
-
-interface DummyRepository {
-    suspend fun getDummies(request: Dummy): Result<DummyResultModel>
-}
