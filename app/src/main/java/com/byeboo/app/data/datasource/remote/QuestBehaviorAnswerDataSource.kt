ea5900ad3e47cfc0d0702b850b19bfe5225e7814
JAY <wndkdus102@naver.com>
2025-07-15 18:44:45 +0900

    #60 [feat] : DataSource, DataSourceImpl 작성

--- app/src/main/java/com/byeboo/app/data/datasource/remote/QuestBehaviorAnswerDataSource.kt
+++ app/src/main/java/com/byeboo/app/data/datasource/remote/QuestBehaviorAnswerDataSource.kt
@@ -4,6 +4,7 @@ import com.byeboo.app.data.dto.base.BaseResponse
 import com.byeboo.app.data.dto.base.NullableBaseResponse
 import com.byeboo.app.data.dto.request.QuestBehaviorAnswerRequestDto
 import com.byeboo.app.data.dto.request.QuestSignedUrlRequestDto
+import com.byeboo.app.data.dto.response.QuestBehaviorAnswerResponseDto
 import com.byeboo.app.data.dto.response.QuestSingedUrlResponseDto
 import okhttp3.RequestBody
 import retrofit2.Response
@@ -12,4 +13,5 @@ interface QuestBehaviorAnswerDataSource {
     suspend fun postQuestSignedUrl(request: QuestSignedUrlRequestDto) : BaseResponse<QuestSingedUrlResponseDto>
     suspend fun putImageToSignedUrl(signedUrl: String, requestBody : RequestBody) : Response<Unit>
     suspend fun postQuestBehaviorAnswer(questId: Long, request: QuestBehaviorAnswerRequestDto) : NullableBaseResponse<Unit>
+    suspend fun getQuestBehaviorAnswer(questId: Long): BaseResponse<QuestBehaviorAnswerResponseDto>
 }
--- app/src/main/java/com/byeboo/app/data/datasourceimpl/remote/QuestBehaviorAnswerDataSourceImpl.kt
+++ app/src/main/java/com/byeboo/app/data/datasourceimpl/remote/QuestBehaviorAnswerDataSourceImpl.kt
@@ -5,8 +5,9 @@ import com.byeboo.app.data.dto.base.BaseResponse
 import com.byeboo.app.data.dto.base.NullableBaseResponse
 import com.byeboo.app.data.dto.request.QuestBehaviorAnswerRequestDto
 import com.byeboo.app.data.dto.request.QuestSignedUrlRequestDto
+import com.byeboo.app.data.dto.response.QuestBehaviorAnswerResponseDto
 import com.byeboo.app.data.dto.response.QuestSingedUrlResponseDto
-import com.byeboo.app.data.service.QuestBehaviorService
+import com.byeboo.app.data.service.quest.QuestBehaviorService
 import okhttp3.RequestBody
 import retrofit2.Response
 import javax.inject.Inject
@@ -28,5 +29,9 @@ class QuestBehaviorAnswerDataSourceImpl @Inject constructor(
         request: QuestBehaviorAnswerRequestDto
     ): NullableBaseResponse<Unit> {
         return questBehaviorService.postQuestAnswer(questId = questId, request = request)
+    }
+
+    override suspend fun getQuestBehaviorAnswer(questId: Long): BaseResponse<QuestBehaviorAnswerResponseDto> {
+        return questBehaviorService.getQuestBehaviorAnswer(questId = questId)
     }
 }
