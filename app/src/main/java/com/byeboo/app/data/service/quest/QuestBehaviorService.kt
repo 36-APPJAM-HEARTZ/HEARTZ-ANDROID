839cc8e0ac09dacc8dc904f51bd54911c59f9401
JAY <wndkdus102@naver.com>
2025-07-15 18:43:09 +0900

    #60 [feat] : Service 작성

---
+++ app/src/main/java/com/byeboo/app/data/service/quest/QuestBehaviorService.kt
@@ -0,0 +1,42 @@
+package com.byeboo.app.data.service.quest
+
+import com.byeboo.app.data.dto.base.BaseResponse
+import com.byeboo.app.data.dto.base.NullableBaseResponse
+import com.byeboo.app.data.dto.request.QuestBehaviorAnswerRequestDto
+import com.byeboo.app.data.dto.request.QuestSignedUrlRequestDto
+import com.byeboo.app.data.dto.response.QuestBehaviorAnswerResponseDto
+import com.byeboo.app.data.dto.response.QuestSingedUrlResponseDto
+import okhttp3.RequestBody
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Path
+import retrofit2.http.Url
+
+interface QuestBehaviorService {
+
+    @POST("/api/v1/quests/images/signed-url")
+    suspend fun postQuestSignedUrl(
+        @Body request: QuestSignedUrlRequestDto
+    ): BaseResponse<QuestSingedUrlResponseDto>
+
+    @PUT
+    suspend fun putImageToUrl(
+        @Url presignedUrl: String,
+        @Body requestBody: RequestBody,
+    ): Response<Unit>
+
+    @POST("/api/v1/quests/{questId}/active")
+    suspend fun postQuestAnswer(
+        @Path("questId") questId: Long,
+        @Body request: QuestBehaviorAnswerRequestDto
+    ): NullableBaseResponse<Unit>
+
+    @GET("/api/v1/quests/answer/{questId}")
+    suspend fun getQuestBehaviorAnswer(
+        @Path("questId") questId: Long,
+    ): BaseResponse<QuestBehaviorAnswerResponseDto>
+
+}
