352d8a96f23daa2114c17a9c44649a7ca06f9606
sohee <144779368+sohee6989@users.noreply.github.com>
2025-07-16 00:42:27 +0900

    #81 [mod] : QuestReview 네비 설정 변경

--- app/src/main/java/com/byeboo/app/presentation/quest/QuestState.kt
+++ app/src/main/java/com/byeboo/app/presentation/quest/QuestState.kt
@@ -1,7 +1,5 @@
 package com.byeboo.app.presentation.quest
 
-import com.byeboo.app.core.model.QuestType
-
 sealed class QuestState {
     object Available : QuestState()
     object Complete : QuestState()
@@ -13,6 +11,6 @@ sealed interface QuestSideEffect {
     data class NavigateToQuestTip(val questId: Long) : QuestSideEffect
     data class NavigateToQuestRecording(val questId: Long) : QuestSideEffect
     data class NavigateToQuestBehavior(val questId: Long) : QuestSideEffect
-    data class NavigateToQuestReview(val questId: Long, val type: QuestType) : QuestSideEffect
+    data class NavigateToQuestReview(val questId: Long) : QuestSideEffect
     data object NavigateToHome : QuestSideEffect
 }
